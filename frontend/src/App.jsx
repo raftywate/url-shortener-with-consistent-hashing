d00c77a2bb542078743037f1a16f029f2d96fb5c
Abhishek Kumar Gupta <106859081+raftywate@users.noreply.github.com>
2026-05-22 12:25:54 +0530

    Restructured repository into backend and frontend modules

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+export default function App() {
+
+  return (
+
+    <div className="min-h-screen bg-black text-white">
+
+      <div className="max-w-6xl mx-auto px-6 py-10">
+
+        <h1 className="text-5xl font-bold mb-4">
+          Distributed URL Shortener
+        </h1>
+
+        <p className="text-gray-400 text-lg">
+          Consistent Hashing + Redis + Sharding
+        </p>
+
+      </div>
+
+    </div>
+  )
+}
