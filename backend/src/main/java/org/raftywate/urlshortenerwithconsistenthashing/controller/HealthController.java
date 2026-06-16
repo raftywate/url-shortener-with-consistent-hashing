4075a3c4eaff8dfa78c170e47eb634b9a0b2bed1
Abhishek Kumar Gupta <106859081+raftywate@users.noreply.github.com>
2026-06-16 22:15:45 +0530

    added health endpoint

---
+++ backend/src/main/java/org/raftywate/urlshortenerwithconsistenthashing/controller/HealthController.java
@@ -0,0 +1,15 @@
+package org.raftywate.urlshortenerwithconsistenthashing.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+
+        return "OK";
+
+    }
+}
